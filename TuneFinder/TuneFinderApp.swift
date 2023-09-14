bfdd388566b6403ef98bb16b44b76f24fec75f06
tony <tonykam@cherrypicks.com>
2023-09-14 22:33:36 +0800

    Initial Commit

---
+++ TuneFinder/TuneFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneFinderApp.swift
+//  TuneFinder
+//
+//  Created by Tony Kam on 14/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct TuneFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
